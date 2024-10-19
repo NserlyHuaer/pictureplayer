85e9cb8886395950717ef1dc167167588dff4d16
nserly <14988414+nserly-huaer@user.noreply.gitee.com>
2024-10-19 15:21:53 +0800

    push

--- src/main/java/Component/FileManagementFrame.java
+++ src/main/java/Component/FileManagementFrame.java
@@ -8,16 +8,8 @@ import javax.swing.*;
 public class FileManagementFrame extends JDialog {
     private static final ClosingListener closingListener = new ClosingListener();
 
-    public void setVisible(boolean b) {
-        super.setVisible(b);
-        if (b) {
-            addKeyListener(closingListener);
-        }
-    }
-
-    public void dispose() {
-        super.dispose();
-        removeKeyListener(closingListener);
+    public FileManagementFrame() {
+        addKeyListener(closingListener);
     }
 
 }
