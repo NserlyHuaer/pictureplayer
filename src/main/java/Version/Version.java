e19e11fa2a9ca16f57eb3ab8bc8da3f44ef43d0d
nserly <14988414+nserly-huaer@user.noreply.gitee.com>
2024-10-19 15:56:25 +0800

    push

--- src/main/java/Version/Version.java
+++ src/main/java/Version/Version.java
@@ -1,8 +1,14 @@
 package Version;
 
 public class Version {
-    private static final String version = "1.0.0 beta";
-    public static String getVersion(){
+    private static final String version = "1.0.0 beta1";
+    private static final long VersionID = 1255L;
+
+    public static String getVersion() {
         return version;
+    }
+
+    public static long getVersionID() {
+        return VersionID;
     }
 }
