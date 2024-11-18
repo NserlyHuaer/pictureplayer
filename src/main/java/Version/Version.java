2ea01b0b57cc920dfd42ea237e1b601aea0db658
nserly <fakagsjf@outlook.com>
2024-11-18 22:26:39 +0800

    push

--- src/main/java/Version/Version.java
+++ src/main/java/Version/Version.java
@@ -2,7 +2,7 @@ package Version;
 
 public class Version {
     private static final String version = "1.0.0 beta6";
-    private static final String VersionID = "1275";
+    private static final String VersionID = "1277";
 
     public static String getVersion() {
         return version;
